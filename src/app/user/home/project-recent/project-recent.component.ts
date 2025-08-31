f9cf35f093637b25c8dcbc93bc6293d3b1469fee
awfaabdulkader <awfaabdulkader6@gmail.com>
2025-08-31 20:28:18 +0100

    first commit

---
+++ src/app/user/home/project-recent/project-recent.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-recent',
+  imports: [],
+  templateUrl: './project-recent.component.html',
+  styles: ``
+})
+export class ProjectRecentComponent {
+
+}
---
+++ src/app/user/home/work-with-me/work-with-me.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-with-me',
+  imports: [],
+  templateUrl: './work-with-me.component.html',
+  styles: ``
+})
+export class WorkWithMeComponent {
+
+}
